c1820f33d47e123376118a0f901bd0015501733c
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-06-12 16:50:47 +0300

    Add project files.

---
+++ MyArray/Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArray
+{
+    internal class Array
+    {
+        private int[] array;
+        public Array()
+        {
+            array = new int[0];
+        }
+        public void Print()
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + "\t");
+            }
+            Console.WriteLine();
+        }
+        public void Clear()
+        {
+            array = new int[0];
+        }
+    }
+}
