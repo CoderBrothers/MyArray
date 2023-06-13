1e4acc4ff38f3c0edf16feb345d6263e6b35e807
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-06-13 18:09:18 +0300

    Added code

--- MyArray/Array.cs
+++ MyArray/Array.cs
@@ -25,5 +25,32 @@ namespace MyArray
         {
             array = new int[0];
         }
+        public void Add(ref int[] array, int value, int index)
+        {
+            int[] newArray = new int[array.Length + 1];
+            newArray[index] = value;
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = array[i];
+            }
+            for (int i = index; i < array.Length; i++)
+            {
+                newArray[i + 1] = array[i];
+            }
+            array = newArray;
+        }
+        public void Remove(ref int[] arrray, int index)
+        {
+            int[]newArray = new int[arrray.Length - 1];
+            for (int i = 0;i < index; i++)
+            {
+                newArray[i] = arrray[i];
+            }
+            for (int i = index + 1; i < array.Length; i++)
+            {
+                newArray[i - 1] = array[i];
+            }
+            array = newArray;
+        }
     }
 }
