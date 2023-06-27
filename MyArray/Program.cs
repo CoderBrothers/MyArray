3c7f2eaf6a69bde28d9b1dfddc4d8b0a52bf72a7
LuckyCoder-max <zilli7733@gmail.com>
2023-06-27 18:45:29 +0300

    Testing

--- MyArray/Program.cs
+++ MyArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Collections;
+using System.Collections;
+
 namespace MyArray
 {
     internal class Program
@@ -6,6 +8,7 @@ namespace MyArray
         static void Main(string[] args)
         {
             Array<int> array = new();
+            Collections.Stack<int> stack = new();
             array.Add(1);
             array.Add(3);
             array.Add(2);
@@ -13,15 +16,13 @@ namespace MyArray
             array.Add(3);
             array.Add(4);
             array.Add(5);
-	        array.Add(5);
+            array.Add(5);
             array.Add(5);
             array.Add(1);
             array.Remove(1);
-            array.Print();
             //Console.WriteLine(array.RemoveAt(2));
             //Console.WriteLine(array.Insert(111, 2));
             array.Pop();
-            array.Print();
             foreach (int i in array)
             {
                 Console.Write(i + " ");
@@ -34,7 +35,47 @@ namespace MyArray
             }
             Console.WriteLine();
             Console.WriteLine(array[1]);
-	    Console.WriteLine(array[0]);
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array.IsEmpty());
+            array.Clear();
+            Console.WriteLine(array.IsEmpty());
+            Console.WriteLine();
+
+
+            stack.Push(1);
+            stack.Push(2);
+            Console.WriteLine(stack.Peek());
+            foreach (int i in stack)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(stack.IsEmpty());
+            stack.Clear();
+            Console.WriteLine(stack.IsEmpty());
+            //foreach (int i in stack)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine(stack.Peek());
+
+
+
+            //System.Collections.Generic.Stack<int> stack2 = new();
+            //stack2.Push(2);
+            //stack2.Push(3);
+            //foreach (int i in stack2)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+            //stack2.Clear();
+            //foreach (int i in stack2)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(stack2.Peek());
         }
     }
 }
