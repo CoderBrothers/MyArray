724dc6a1b8bee0d264594aca88bba8d5603c3af7
LuckyCoder-max <zilli7733@gmail.com>
2023-06-27 18:49:19 +0300

    Added Stack class

--- MyLib/Stack.cs
+++ MyLib/Stack.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections;
 namespace Collections
 {
-    public class Stack<T>
+    public class Stack<T> : IEnumerable<T>
     {
+        private Array<T> _stack;
 
+        public Stack()
+        {
+            _stack = new Array<T>();
+        }
+        public void Clear() => _stack.Clear();
+        public void Push(T value) => _stack.Add(value);
+        public T Peek() => _stack[_stack.Length - 1];
+        public T Pop() => _stack.Pop();
+        public bool Contains(T value) => _stack.Contains(value);
+        public bool IsEmpty() => _stack.IsEmpty();
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_stack).GetEnumerator();
+        IEnumerator System.Collections.IEnumerable.GetEnumerator() => _stack.GetEnumerator();
     }
 }
